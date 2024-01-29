49039621fc5469613a5acd83be4087cc0c62d016
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-01-29 13:50:30 -0300

    feat: create header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import { Timer, Scroll } from 'phosphor-react'
+import { HeaderContainer } from './styles'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>logo</span>
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
