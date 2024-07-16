0fcb1e7b40e98373e665c9fbba12b59c7ca79d38
joaohenriquefernandes <joaohenriquefernandes.dev@gmail.com>
2024-07-16 09:12:32 -0300

    feat: create db in local storage

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
-import React, { createContext, useReducer, useState } from 'react';
+import { differenceInSeconds } from 'date-fns';
+import React, { createContext, useEffect, useReducer, useState } from 'react';
 
 import {
   addNewCycleAction,
@@ -33,16 +34,42 @@ export const CyclesContext = createContext({} as ICycleContextType);
 export function CyclesContextProvider({
   children,
 }: ICycleContextProviderProps) {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  });
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    (initialState) => {
+      const storedStateAsJSON = localStorage.getItem(
+        '@ignite-timer:cycle-state-1.0.0',
+      );
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+      if (storedStateAsJSON) {
+        return JSON.parse(storedStateAsJSON);
+      }
+
+      return initialState;
+    },
+  );
 
   const { activeCycleId, cycles } = cyclesState;
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate));
+    }
+
+    return 0;
+  });
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState);
+
+    localStorage.setItem('@ignite-timer:cycle-state-1.0.0', stateJSON);
+  }, [cyclesState]);
 
   function setSecondsPassed(seconds: number) {
     setAmountSecondsPassed(seconds);
